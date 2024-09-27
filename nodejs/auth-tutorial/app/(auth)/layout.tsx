8478418da8fbdbb0e3f79759806b40136692d483
xianjun-zhang <zxj.job@outlook.com>
2024-09-26 23:08:11 -0700

    Router目录学习
    1. 如果不想所有的子页面访问url必须带父目录，可以用`()`将父目录包裹， 这样所有子页面访问url就不需要带父目录了。比如：`/auth/login`, `/auth/register`, 用把文件夹名字`auth`包裹在()里变成`(auth)`， 这样访问的时候只需要 `/login`, `/register`即可
    2. 由于app下面的目录默认作为router， 在web url可访问，有些目录我们只是作为内部使用，不想错误的对外暴露，可以用`_`来放在目录前，比如`_components`， 这样外部url就无法访问，显示404错误。同时， 在app目录下， 可以自由调用`components`目录下的组件。

--- nodejs/auth-tutorial/app/(auth)/layout.tsx
+++ nodejs/auth-tutorial/app/(auth)/layout.tsx
@@ -4,8 +4,11 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div>
             <nav className='flex justify-start items-center p-4 space-x-4'>
-                <Link href="/auth/login" className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-300 transition-colors duration-200">
+                <Link href="/login" className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-300 transition-colors duration-200">
                     NavBar: Login
+                </Link>
+                <Link href="/register" className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-300 transition-colors duration-200">
+                    NavBar: Register
                 </Link>
             </nav>
             {children}
---
+++ nodejs/auth-tutorial/app/_components/page.tsx
@@ -0,0 +1,9 @@
+const ComponentPage = () => {
+    return (
+        <div>
+            <h1>Component Page</h1>
+        </div>
+    );
+};
+
+export default ComponentPage;
